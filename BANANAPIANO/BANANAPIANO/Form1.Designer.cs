650a29f4f223ec6dce16c4492223f36c0f89ae44
Lacier <tuankietvn@outlook.com>
2019-05-10 14:28:06 +0700

    BANANAPIANO

---
+++ BANANAPIANO/BANANAPIANO/Form1.Designer.cs
@@ -0,0 +1,393 @@
+﻿namespace BANANAPIANO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.bl8 = new System.Windows.Forms.PictureBox();
+            this.bl7 = new System.Windows.Forms.PictureBox();
+            this.bl6 = new System.Windows.Forms.PictureBox();
+            this.bl1 = new System.Windows.Forms.PictureBox();
+            this.bl5 = new System.Windows.Forms.PictureBox();
+            this.bl4 = new System.Windows.Forms.PictureBox();
+            this.bl3 = new System.Windows.Forms.PictureBox();
+            this.bl2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.wh7 = new System.Windows.Forms.PictureBox();
+            this.wh6 = new System.Windows.Forms.PictureBox();
+            this.wh5 = new System.Windows.Forms.PictureBox();
+            this.wh4 = new System.Windows.Forms.PictureBox();
+            this.wh3 = new System.Windows.Forms.PictureBox();
+            this.wh2 = new System.Windows.Forms.PictureBox();
+            this.wh1 = new System.Windows.Forms.PictureBox();
+            this.note1 = new System.Windows.Forms.Label();
+            this.note2 = new System.Windows.Forms.Label();
+            this.note3 = new System.Windows.Forms.Label();
+            this.note4 = new System.Windows.Forms.Label();
+            this.note5 = new System.Windows.Forms.Label();
+            this.note6 = new System.Windows.Forms.Label();
+            this.note7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.bl8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bl7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bl6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bl5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bl4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bl3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bl2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wh7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wh6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wh5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wh4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wh3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wh2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wh1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bl8
+            // 
+            this.bl8.BackColor = System.Drawing.Color.Black;
+            this.bl8.Location = new System.Drawing.Point(686, 162);
+            this.bl8.Name = "bl8";
+            this.bl8.Size = new System.Drawing.Size(25, 183);
+            this.bl8.TabIndex = 31;
+            this.bl8.TabStop = false;
+            // 
+            // bl7
+            // 
+            this.bl7.BackColor = System.Drawing.Color.Black;
+            this.bl7.Location = new System.Drawing.Point(587, 162);
+            this.bl7.Name = "bl7";
+            this.bl7.Size = new System.Drawing.Size(52, 183);
+            this.bl7.TabIndex = 30;
+            this.bl7.TabStop = false;
+            // 
+            // bl6
+            // 
+            this.bl6.BackColor = System.Drawing.Color.Black;
+            this.bl6.Location = new System.Drawing.Point(487, 162);
+            this.bl6.Name = "bl6";
+            this.bl6.Size = new System.Drawing.Size(52, 183);
+            this.bl6.TabIndex = 29;
+            this.bl6.TabStop = false;
+            // 
+            // bl1
+            // 
+            this.bl1.BackColor = System.Drawing.Color.Black;
+            this.bl1.Location = new System.Drawing.Point(12, 162);
+            this.bl1.Name = "bl1";
+            this.bl1.Size = new System.Drawing.Size(25, 183);
+            this.bl1.TabIndex = 28;
+            this.bl1.TabStop = false;
+            // 
+            // bl5
+            // 
+            this.bl5.BackColor = System.Drawing.Color.Black;
+            this.bl5.Location = new System.Drawing.Point(386, 162);
+            this.bl5.Name = "bl5";
+            this.bl5.Size = new System.Drawing.Size(52, 183);
+            this.bl5.TabIndex = 27;
+            this.bl5.TabStop = false;
+            // 
+            // bl4
+            // 
+            this.bl4.BackColor = System.Drawing.Color.Black;
+            this.bl4.Location = new System.Drawing.Point(286, 162);
+            this.bl4.Name = "bl4";
+            this.bl4.Size = new System.Drawing.Size(52, 183);
+            this.bl4.TabIndex = 26;
+            this.bl4.TabStop = false;
+            // 
+            // bl3
+            // 
+            this.bl3.BackColor = System.Drawing.Color.Black;
+            this.bl3.Location = new System.Drawing.Point(186, 162);
+            this.bl3.Name = "bl3";
+            this.bl3.Size = new System.Drawing.Size(52, 183);
+            this.bl3.TabIndex = 25;
+            this.bl3.TabStop = false;
+            // 
+            // bl2
+            // 
+            this.bl2.BackColor = System.Drawing.Color.Black;
+            this.bl2.Location = new System.Drawing.Point(85, 162);
+            this.bl2.Name = "bl2";
+            this.bl2.Size = new System.Drawing.Size(52, 183);
+            this.bl2.TabIndex = 24;
+            this.bl2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Myriad Pro Cond", 71.99999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Gold;
+            this.label1.Location = new System.Drawing.Point(102, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(551, 116);
+            this.label1.TabIndex = 23;
+            this.label1.Text = "BANANA PIANO";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // wh7
+            // 
+            this.wh7.BackColor = System.Drawing.Color.White;
+            this.wh7.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("wh7.BackgroundImage")));
+            this.wh7.Image = global::BANANAPIANO.Properties.Resources.WhiteNotes;
+            this.wh7.Location = new System.Drawing.Point(612, 162);
+            this.wh7.Name = "wh7";
+            this.wh7.Size = new System.Drawing.Size(100, 276);
+            this.wh7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.wh7.TabIndex = 22;
+            this.wh7.TabStop = false;
+            // 
+            // wh6
+            // 
+            this.wh6.BackColor = System.Drawing.Color.White;
+            this.wh6.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("wh6.BackgroundImage")));
+            this.wh6.Image = global::BANANAPIANO.Properties.Resources.WhiteNotes;
+            this.wh6.Location = new System.Drawing.Point(512, 162);
+            this.wh6.Name = "wh6";
+            this.wh6.Size = new System.Drawing.Size(100, 276);
+            this.wh6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.wh6.TabIndex = 21;
+            this.wh6.TabStop = false;
+            // 
+            // wh5
+            // 
+            this.wh5.BackColor = System.Drawing.Color.White;
+            this.wh5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("wh5.BackgroundImage")));
+            this.wh5.Image = global::BANANAPIANO.Properties.Resources.WhiteNotes;
+            this.wh5.Location = new System.Drawing.Point(412, 162);
+            this.wh5.Name = "wh5";
+            this.wh5.Size = new System.Drawing.Size(100, 276);
+            this.wh5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.wh5.TabIndex = 20;
+            this.wh5.TabStop = false;
+            // 
+            // wh4
+            // 
+            this.wh4.BackColor = System.Drawing.Color.White;
+            this.wh4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("wh4.BackgroundImage")));
+            this.wh4.Image = global::BANANAPIANO.Properties.Resources.WhiteNotes;
+            this.wh4.Location = new System.Drawing.Point(312, 162);
+            this.wh4.Name = "wh4";
+            this.wh4.Size = new System.Drawing.Size(100, 276);
+            this.wh4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.wh4.TabIndex = 19;
+            this.wh4.TabStop = false;
+            // 
+            // wh3
+            // 
+            this.wh3.BackColor = System.Drawing.Color.White;
+            this.wh3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("wh3.BackgroundImage")));
+            this.wh3.Image = global::BANANAPIANO.Properties.Resources.WhiteNotes;
+            this.wh3.Location = new System.Drawing.Point(212, 162);
+            this.wh3.Name = "wh3";
+            this.wh3.Size = new System.Drawing.Size(100, 276);
+            this.wh3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.wh3.TabIndex = 18;
+            this.wh3.TabStop = false;
+            // 
+            // wh2
+            // 
+            this.wh2.BackColor = System.Drawing.Color.White;
+            this.wh2.Image = global::BANANAPIANO.Properties.Resources.WhiteNotes;
+            this.wh2.Location = new System.Drawing.Point(112, 162);
+            this.wh2.Name = "wh2";
+            this.wh2.Size = new System.Drawing.Size(100, 276);
+            this.wh2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.wh2.TabIndex = 17;
+            this.wh2.TabStop = false;
+            // 
+            // wh1
+            // 
+            this.wh1.BackColor = System.Drawing.Color.White;
+            this.wh1.Image = global::BANANAPIANO.Properties.Resources.WhiteNotes;
+            this.wh1.Location = new System.Drawing.Point(12, 162);
+            this.wh1.Name = "wh1";
+            this.wh1.Size = new System.Drawing.Size(100, 276);
+            this.wh1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.wh1.TabIndex = 16;
+            this.wh1.TabStop = false;
+            // 
+            // note1
+            // 
+            this.note1.AutoSize = true;
+            this.note1.BackColor = System.Drawing.Color.White;
+            this.note1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.note1.Location = new System.Drawing.Point(40, 393);
+            this.note1.Name = "note1";
+            this.note1.Size = new System.Drawing.Size(35, 31);
+            this.note1.TabIndex = 32;
+            this.note1.Text = "C";
+            // 
+            // note2
+            // 
+            this.note2.AutoSize = true;
+            this.note2.BackColor = System.Drawing.Color.White;
+            this.note2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.note2.Location = new System.Drawing.Point(143, 393);
+            this.note2.Name = "note2";
+            this.note2.Size = new System.Drawing.Size(35, 31);
+            this.note2.TabIndex = 32;
+            this.note2.Text = "D";
+            // 
+            // note3
+            // 
+            this.note3.AutoSize = true;
+            this.note3.BackColor = System.Drawing.Color.White;
+            this.note3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.note3.Location = new System.Drawing.Point(246, 393);
+            this.note3.Name = "note3";
+            this.note3.Size = new System.Drawing.Size(33, 31);
+            this.note3.TabIndex = 32;
+            this.note3.Text = "E";
+            // 
+            // note4
+            // 
+            this.note4.AutoSize = true;
+            this.note4.BackColor = System.Drawing.Color.White;
+            this.note4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.note4.Location = new System.Drawing.Point(346, 393);
+            this.note4.Name = "note4";
+            this.note4.Size = new System.Drawing.Size(32, 31);
+            this.note4.TabIndex = 32;
+            this.note4.Text = "F";
+            // 
+            // note5
+            // 
+            this.note5.AutoSize = true;
+            this.note5.BackColor = System.Drawing.Color.White;
+            this.note5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.note5.Location = new System.Drawing.Point(447, 393);
+            this.note5.Name = "note5";
+            this.note5.Size = new System.Drawing.Size(36, 31);
+            this.note5.TabIndex = 32;
+            this.note5.Text = "G";
+            // 
+            // note6
+            // 
+            this.note6.AutoSize = true;
+            this.note6.BackColor = System.Drawing.Color.White;
+            this.note6.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.note6.Location = new System.Drawing.Point(544, 393);
+            this.note6.Name = "note6";
+            this.note6.Size = new System.Drawing.Size(33, 31);
+            this.note6.TabIndex = 32;
+            this.note6.Text = "A";
+            // 
+            // note7
+            // 
+            this.note7.AutoSize = true;
+            this.note7.BackColor = System.Drawing.Color.White;
+            this.note7.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.note7.Location = new System.Drawing.Point(646, 393);
+            this.note7.Name = "note7";
+            this.note7.Size = new System.Drawing.Size(33, 31);
+            this.note7.TabIndex = 32;
+            this.note7.Text = "B";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(726, 450);
+            this.Controls.Add(this.note7);
+            this.Controls.Add(this.note6);
+            this.Controls.Add(this.note5);
+            this.Controls.Add(this.note4);
+            this.Controls.Add(this.note3);
+            this.Controls.Add(this.note2);
+            this.Controls.Add(this.note1);
+            this.Controls.Add(this.bl8);
+            this.Controls.Add(this.bl7);
+            this.Controls.Add(this.bl6);
+            this.Controls.Add(this.bl1);
+            this.Controls.Add(this.bl5);
+            this.Controls.Add(this.bl4);
+            this.Controls.Add(this.bl3);
+            this.Controls.Add(this.bl2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.wh7);
+            this.Controls.Add(this.wh6);
+            this.Controls.Add(this.wh5);
+            this.Controls.Add(this.wh4);
+            this.Controls.Add(this.wh3);
+            this.Controls.Add(this.wh2);
+            this.Controls.Add(this.wh1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.bl8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bl7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bl6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bl5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bl4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bl3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bl2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wh7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wh6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wh5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wh4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wh3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wh2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wh1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox bl8;
+        private System.Windows.Forms.PictureBox bl7;
+        private System.Windows.Forms.PictureBox bl6;
+        private System.Windows.Forms.PictureBox bl1;
+        private System.Windows.Forms.PictureBox bl5;
+        private System.Windows.Forms.PictureBox bl4;
+        private System.Windows.Forms.PictureBox bl3;
+        private System.Windows.Forms.PictureBox bl2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox wh7;
+        private System.Windows.Forms.PictureBox wh6;
+        private System.Windows.Forms.PictureBox wh5;
+        private System.Windows.Forms.PictureBox wh4;
+        private System.Windows.Forms.PictureBox wh3;
+        private System.Windows.Forms.PictureBox wh2;
+        private System.Windows.Forms.PictureBox wh1;
+        private System.Windows.Forms.Label note1;
+        private System.Windows.Forms.Label note2;
+        private System.Windows.Forms.Label note3;
+        private System.Windows.Forms.Label note4;
+        private System.Windows.Forms.Label note5;
+        private System.Windows.Forms.Label note6;
+        private System.Windows.Forms.Label note7;
+    }
+}
+
---
+++ BANANAPIANO/BANANAPIANO/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BANANAPIANO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+            if (e.KeyChar == 'q')
+            {
+                PlayWav(Properties.Resources.A, false);
+            }
+            else if (e.KeyChar == 'w')
+            {
+                PlayWav(Properties.Resources.B, false);
+            }
+            else if (e.KeyChar == 'e')
+            {
+                PlayWav(Properties.Resources.C, false);
+            }
+            else if (e.KeyChar == 'r')
+            {
+                PlayWav(Properties.Resources.D, false);
+            }
+            else if (e.KeyChar == 't')
+            {
+                PlayWav(Properties.Resources.E, false);
+            }
+            else if (e.KeyChar == 'y')
+            {
+                PlayWav(Properties.Resources.F, false);
+            }
+            else if (e.KeyChar == 'u')
+            {
+                PlayWav(Properties.Resources.G, false);
+            }
+        }
+
+        private SoundPlayer pl = null;
+
+        private void PlayWav(Stream stream, bool play_looping)
+        {
+            // Stop the player if it is running.
+            if (pl != null)
+            {
+                pl.Stop();
+                pl.Dispose();
+                pl = null;
+            }
+            
+            if (stream == null) return;
+            
+            pl = new SoundPlayer(stream);
+            
+            if (play_looping)
+                pl.PlayLooping();
+            else
+                pl.Play();
+        }
+    }
+}
